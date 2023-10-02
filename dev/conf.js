466cdd7bdb858f3eac5191444cc9aaf583df7d7a
PirateSU <101900500+PirateSU@users.noreply.github.com>
2023-10-02 04:05:32 +0300

    Update conf.js

--- dev/conf.js
+++ dev/conf.js
@@ -1,9 +1,9 @@
 window.appConf = {
     author: 'React Drive CMS',
-    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    dashboardId: '1Ud0VYbBrHHl0t1Nrzv_RDeHoMc3QZukOn9lioOroqeM',
     sendContactMessageUrlId:
         'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
     shortname: 'easydrivecms',
     ipInfoToken: '6efb7a808a4857',
-    googleApiKey: 'AIzaSyBYOwC55SpcCZaG9d87UuHkxkQ8GRI_39M',
+    googleApiKey: 'AIzaSyBuEER1bRuOb4jvLGAJuj6JZjk7CuPeHew',
 }
